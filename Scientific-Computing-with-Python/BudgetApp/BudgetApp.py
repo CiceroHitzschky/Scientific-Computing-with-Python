a69bdc650ea3af434dbf99839a474b7a968fec50
CiceroHitzschky <hitzschky7@gmail.com>
2022-11-28 22:25:04 -0300

    Anter do Gráfico

--- Scientific-Computing-with-Python/BudgetApp/BudgetApp.py
+++ Scientific-Computing-with-Python/BudgetApp/BudgetApp.py
@@ -3,6 +3,7 @@ class Category:
     
     ledger = list()
     
+    percent = 0
     funds = 0
     
     loss = 0
@@ -60,15 +61,46 @@ class Category:
         else:
             return False
 
-food = Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+# Método extralista 
+def create_spend_chart(list_of_categories = []):
+    if list_of_categories == []:
+        print("Percentage spent by category")
+        for i in range(11):
+            print(f"{abs((i-10))*10:>3}|\n",end='')
+        print(" "*4+'---')
+    else:
+        total = 0
+        for category in list_of_categories:
+            total+= abs(category.loss)
+        for category in list_of_categories:    
+            percent = abs(round(category.loss / total,2))*100
+            category.percent += percent
+        for category in list_of_categories:
+            
+            tabela=f'''
+            {100:>3}|{} {} {}
+            {90:>3}| {} {} {}
+            {80:>3}| {} {} {} 
+            {70:>3}| {} {} {}
+            {60:>3}| {} {} {}
+            {50:>3}| {} {} {}
+            {40:>3}| {} {} {}
+            {30:>3}| {} {} {}
+            {20:>3}| {} {} {}
+            {10:>3}| {} {} {}
+            {0:>3}|  {} {} {}
+            '''
+
+food = Category('food')
+entertainment = Category('entertainment')
+business = Category('business')
+
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
+
+create_spend_chart([food, entertainment, business])
+print("Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  ")
